1eb68820f2d8271ee03573a01377d614b3d70dc2
Ronan Houée <rh.4@laposte.net>
2022-03-05 00:58:41 +0100

    4/03/2022

--- src/script.js
+++ src/script.js
@@ -4,7 +4,9 @@ const searchMotor = document.querySelector("#searchMotor");
 const typeSearch = document.querySelector("#type-search");
 const btnAdvancedForm = document.querySelector("#btnAdvancedSearch");
 const btnTraceRoute = document.querySelector("#btnTraceRoute");
-
+const btnRouteShowOptions = document.querySelector("#btnRouteShowOptions");
+const btnRouteHideOptions = document.querySelector("#btnRouteHideOptions");
+const btnsTransports = document.querySelectorAll(".btnMeansOfTransport");
 //routing 
 const btOpennRoute = document.querySelector("#btnOpenRoute");
 const btnCloseRoute = document.querySelector("#btnCloseRoute");
@@ -15,8 +17,9 @@ const routeDestinationInput = document.querySelector("#routeDestinationInput");
 
 const accessToken = "pk.eyJ1IjoiYWxwaG9uc2VhbG9uenkiLCJhIjoiY2t6aWZ2ZGlzMDg5dzJvbDQyY3JldTAzeiJ9.y_8yWktMT0rmyYFi19aJig";
 const locationList = [null,null];
-const tempMarkers = [];
 const routesList = [];
+let meanOfTransport = undefined;
+
 let map;
 
 const init = function () {
@@ -34,7 +37,7 @@ const init = function () {
         
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            minZoom:1,
+            minZoom:3,
             maxZoom: 10,
             id: 'mapbox/streets-v11',
             tileSize: 512,
@@ -44,12 +47,20 @@ const init = function () {
 
         map.on("click", onMapClick);
     
+        const southWest = L.latLng(-89.98155760646617, -180);
+        const northEast = L.latLng(89.99346179538875, 180);
+        const bounds = L.latLngBounds(southWest, northEast);
+
+        map.setMaxBounds(bounds);
+        map.on('drag', function() {
+	        map.panInsideBounds(bounds, { animate: false });
+        });
     })
 }
 window.onload = init();
 
 formSearch.addEventListener("submit",function(e) {
-    e.preventDefault();aaaaaaaaaaaaaaa
+    e.preventDefault();
     
     //${typeSearch.value}
     const query = `https://nominatim.openstreetmap.org/search?q=${searchMotor.value}&limit=1&format=json&addressdetails=1`;
@@ -78,20 +89,16 @@ formSearch.addEventListener("submit",function(e) {
         searchMotor.value="";
     })
 })
-
 btnAdvancedForm.addEventListener("click", function(e) {
     formSearch.hidden = !(formSearch.hidden);
     formSearchAdvanced.hidden = !(formSearchAdvanced.hidden);
 });
-
-
 btnOpenRoute.addEventListener("click", function(e) {
     
     routeInterface.hidden = false;
     this.hidden = true;  
     traceRoute();  
 });
-
 btnCloseRoute.addEventListener("click",function(e) {
     routeInterface.hidden = true;
     btnOpenRoute.hidden = false;
@@ -99,73 +106,107 @@ btnCloseRoute.addEventListener("click",function(e) {
     locationList.splice(0,locationList.length,null);
     routeStartInput.value = "";
     routeDestinationInput.value="";
+    meanOfTransport = undefined;
+    unselectTransportButton();
 });
-
 btnTraceRoute.addEventListener("click",function(e) {
-    traceRoute();
+    traceRoute(meanOfTransport);
 });
-
 const onMarkerClick = function(e) {
     e.preventDefault();
 }
-
 const onMapClick = function(e) {
-    var container = L.DomUtil.create('div'),
-    startBtn = createButton('Partir d\'ici', container),
-    destBtn = createButton('Aller jusqu\'ici', container);
+    
+    const latClick = e.latlng["lat"];
+    const lngClick = e.latlng["lng"];
+    const query = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngClick},${latClick}.json?access_token=${accessToken}`;
+    fetch(query)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(res) {
+        const container = L.DomUtil.create('div','container');
+        const divInfos = L.DomUtil.create('div','infos');
 
-    L.popup()
-    .setContent(container)
-    .setLatLng(e.latlng)
-    .openOn(map);
+        const divCountry = L.DomUtil.create('div','country');
+        const countryInfos = res.features[res.features.length-1]["place_name"];
+        divCountry.append(countryInfos);
 
-    L.DomEvent.on(startBtn, 'click', function() {
-        locationList.splice(0, 1, e.latlng);
-        const lat = e.latlng["lat"];
-        const lng = e.latlng["lng"];
-        const query = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${accessToken}`;
-        fetch(query)
-        .then(function(res) {
-           return res.json();
-        })
-        .then(function(res) {
-            routeStartInput.value = res.features[2]["text"];
-            btnOpenRoute.click();
-            traceRoute();
-        })
+        const divLocation = L.DomUtil.create('div','location');
+        const locationInfos = res.features[1]["place_name"];
+        divLocation.append(locationInfos);
 
-        map.closePopup();
-    });
+        divInfos.append(divCountry);
+        divInfos.append(divLocation);
+        const btnShowInfos = createButton("Détail",divInfos )
 
-    L.DomEvent.on(destBtn, 'click', function() {
-        locationList.splice(1, 1, e.latlng);
+        const divRouting = L.DomUtil.create('div','routing');
+        const startBtn = createButton('Partir d\'ici',divRouting);
+        const destBtn = createButton('Aller jusqu\'ici', divRouting);
+    
         
+        container.append(divInfos);
+        container.append(divRouting);
+    
 
-        const lat = e.latlng["lat"];
-        const lng = e.latlng["lng"];
-        const query = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${accessToken}`;
-        fetch(query)
-        .then(function(res) {
-           return res.json();
-        })
-        .then(function(res) {
-            routeDestinationInput.value = res.features[2]["text"];
-            btnOpenRoute.click();
-            traceRoute();
-        })
+        L.popup().setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(map);
 
-        map.closePopup();
-    });
+        const loc = document.querySelectorAll(".location");
+        loc.forEach(e => e.hidden = true);
+
+        L.DomEvent.on(startBtn, 'click', function() {
+            locationList.splice(0, 1, e.latlng);
+            const lat = e.latlng["lat"];
+            const lng = e.latlng["lng"];
+            const query = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${accessToken}`;
+            fetch(query)
+            .then(function(res) {
+               return res.json();
+            })
+            .then(function(res) {
+                routeStartInput.value = res.features[res.features.length-1]["text"];
+                btnOpenRoute.click();
+                traceRoute(meanOfTransport);
+            })
+    
+            map.closePopup();
+        });
+
+        L.DomEvent.on(destBtn, 'click', function() {
+            locationList.splice(1, 1, e.latlng);
+            const lat = e.latlng["lat"];
+            const lng = e.latlng["lng"];
+            const query = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${accessToken}`;
+            fetch(query)
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(res) {
+                routeDestinationInput.value = res.features[res.features.length-1]["text"];
+                btnOpenRoute.click();
+                traceRoute(meanOfTransport);
+            })
+
+            map.closePopup();
+        });
+
+        L.DomEvent.on(btnShowInfos, 'click', function() {
+            document.querySelector(".country").hidden = !(document.querySelector(".country").hidden);
+            document.querySelector(".location").hidden = !(document.querySelector(".location").hidden);
+            this.hidden = true;
+        });
+    })
 }
-
 const createButton = function (label, container) {
-    var btn = L.DomUtil.create('button', '', container);
+    const btn = L.DomUtil.create('button', 'btnPopup', container);
     btn.setAttribute('type', 'button');
     btn.innerText = label;
     return btn;
 }
 
-const traceRoute = function() {
+const traceRoute = function(meanOfTransport = "driving-traffic") {
     if (locationList[0] && locationList[1]) {
         routesList.forEach(e => e.remove());
         const control = L.Routing.control({
@@ -173,12 +214,13 @@ const traceRoute = function() {
                 locationList[0],
                 locationList[1]
             ],
-            router: L.Routing.mapbox(accessToken,{language : 'fr'})
+            router: L.Routing.mapbox(accessToken,{profile : `mapbox/${meanOfTransport}`, language : 'fr'})
         });
         routesList.push(control);
         control.addTo(map);
     }
 }
+
 
 routeStartInput.addEventListener("change", function(e) {
     fetch(`https://nominatim.openstreetmap.org/search?q=${e.target.value}&limit=1&format=json&addressdetails=1`)
@@ -194,7 +236,6 @@ routeStartInput.addEventListener("change", function(e) {
     locationList.splice(0,1,coord);
     });
 })
-
 routeDestinationInput.addEventListener("change", function(e) {
     fetch(`https://nominatim.openstreetmap.org/search?q=${e.target.value}&limit=1&format=json&addressdetails=1`)
     .then(function(res) {
@@ -209,3 +250,28 @@ routeDestinationInput.addEventListener("change", function(e) {
     locationList.splice(1,1,coord);
     });
 })
+
+btnRouteShowOptions.addEventListener("click", function(e) {
+    btnRouteHideOptions.hidden = false;
+    btnsTransports.forEach(e => e.hidden = false);
+
+    this.hidden = true;
+});
+
+btnRouteHideOptions.addEventListener("click", function(e) {
+    btnRouteShowOptions.hidden = false;
+    
+    this.hidden = true;
+    btnsTransports.forEach(e => e.hidden = true);
+});
+
+
+btnsTransports.forEach(e => e.addEventListener("click",function(e) {
+    meanOfTransport = e.target.value;
+    unselectTransportButton();
+    e.target.classList.add('selected');
+}))
+
+const unselectTransportButton = function() {
+    btnsTransports.forEach(e => e.classList.remove('selected'))
+}
